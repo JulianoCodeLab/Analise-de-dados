efbdac875cf656493c7aaca59301560e04c65d21
Juliano Ambrósio <gestaojuliano99@gmail.com>
2024-07-13 19:57:59 -0300

    add logo, e formato semi pronto

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
-import dash                                                     # Gerencia a dashboard e cria o servidor 
-from dash import dcc                                            # Responsável por todos os componentes úteis na dash
-from dash import html                                           # Permite colocar códigos HTML na dash
-from dash.dependencies import Input, Output                     # Permite interação com o usuário
-import dash_bootstrap_components as dbc                         # Permite utilizar HTML, CSS e JS para formatar a interface web
+import dash  # Gerencia a dashboard e cria o servidor 
+from dash import dcc  # Responsável por todos os componentes úteis na dash
+from dash import html  # Permite colocar códigos HTML na dash
+from dash.dependencies import Input, Output  # Permite interação com o usuário
+import dash_bootstrap_components as dbc  # Permite utilizar HTML, CSS e JS para formatar a interface web
 
-import plotly.express as px                                     # Permite criação dos gráficos dos plotly de forma mais fácil
-import plotly.graph_objects as go                               # Nos dá mais controle sobre as ferramentas plotly
+import plotly.express as px  # Permite criação dos gráficos dos plotly de forma mais fácil
+import plotly.graph_objects as go  # Nos dá mais controle sobre as ferramentas plotly
 
-import numpy as np                                              # Oferece suporte para array, coleção de funções matemáticas etc.
-import json                                                     # Para leitura de dados ".json"
+import numpy as np  # Oferece suporte para array, coleção de funções matemáticas etc.
+import json  # Para leitura de dados ".json"
 import pandas as pd
 
 # Para ler o CSV
@@ -18,20 +18,25 @@ df_brasil = pd.read_csv("df_brasil.csv")
 df_states_ = df_states[df_states["data"] == "2020-05-13"]
 df_data = df_states[df_states["estado"] == "RJ"]
 
+select_columns = {
+    "casosAcumulado": "Casos Acumulados",
+    "casosNovos": "Novos Casos",
+    "obitosAcumulados": "Óbitos Totais",
+    "obitosNovos": "Óbitos por dia"
+}
+
 # Lê o GeoJSON
 brazil_states = json.load(open("geojson/brazil_geo.json", "r"))
 
-#==================================================================================================
-#----------------------------Iniciando a criação da dash
-
+# Iniciando a criação da dash
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
 # Contém o mapa mais moderno, e melhor para estilizar
 fig = px.choropleth_mapbox(df_states_, locations="estado", color="casosNovos",
-                            center={"lat": -16.95, "lon": -47.78}, zoom=4,
-                            geojson=brazil_states, color_continuous_scale="Redor",
-                            opacity=0.4, hover_data={"casosAcumulado": True, "casosNovos": True, 
-                            "obitosNovos": True, "estado": True})
+                        center={"lat": -16.95, "lon": -47.78}, zoom=4,
+                        geojson=brazil_states, color_continuous_scale="Redor",
+                        opacity=0.4, hover_data={"casosAcumulado": True, "casosNovos": True,
+                                                    "obitosNovos": True, "estado": True})
 
 fig.update_layout(
     paper_bgcolor="#242424",
@@ -52,19 +57,84 @@ fig2.update_layout(
     margin=dict(l=10, r=10, t=10, b=10)
 )
 
-#==================================================================================================
-#----------------------------Layout
-
+# Layout da aplicação
 app.layout = dbc.Container(
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id="line-graph", figure=fig2)
-        ]),
+            html.Div([
+                html.Img(id="logo", src=app.get_asset_url("python-covid.png"), height=50),
+                html.H5("Evolução COVID-19"),
+                dbc.Button("Brasil", color="primary", id="localtion-button", size="lg")
+            ], style={}),
+            html.P("Informe a data na qual deseja obter informações: ", style={"margin-top": "40px"}),
+            html.Div(id="div-test", children=[
+                dcc.DatePickerSingle(
+                    id="date-picker",
+                    min_date_allowed=df_brasil["data"].min(),
+                    max_date_allowed=df_brasil["data"].max(),
+                    initial_visible_month=df_brasil["data"].min(),
+                    date=df_brasil["data"].max(),
+                    display_format="MMMM D, YYYY",
+                    style={"border": "0px solid black"}
+                )
+            ]),
+
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.Span("Casos recuperados"),
+                            html.H3(style={"color": "#adfc92"}, id="casos-recuperados-text"),
+                            html.Span("Em acompanhamento"),
+                            html.H5(id="em-acompanhamento-text"),
+                        ])
+                    ], color="light", outline=True, style={"margin-top": "10px",
+                                                        "box-shadow": "0 4px 4px 0 rgba(0, 0, 0.15), 0 4px 20px 0 rgba(0, 0, 0, 0.19)",
+                                                        "color": "#FFFFFF"})
+                ], md=4),
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.Span("Total de casos confirmados"),
+                            html.H3(style={"color": "#389fd6"}, id="casos-confirmados-text"),
+                            html.Span("Novos casos na data"),
+                            html.H5(id="novos-casos-text"),
+                        ])
+                    ], color="light", outline=True, style={"margin-top": "10px",
+                                                        "box-shadow": "0 4px 4px 0 rgba(0, 0, 0.15), 0 4px 20px 0 rgba(0, 0, 0, 0.19)",
+                                                        "color": "#FFFFFF"})
+                ], md=4),
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.Span("Óbitos confirmados"),
+                            html.H3(style={"color": "#DF2935"}, id="obitos-text"),
+                            html.Span("Óbitos na data"),
+                            html.H5(id="obitos-na-data-text"),
+                        ])
+                    ], color="light", outline=True, style={"margin-top": "10px",
+                                                        "box-shadow": "0 4px 4px 0 rgba(0, 0, 0.15), 0 4px 20px 0 rgba(0, 0, 0, 0.19)",
+                                                        "color": "#FFFFFF"})
+                ], md=4),
+            ]),
+
+            html.Div([
+                html.P("Selecione o tipo de informação: ", style={"margin-top": "25px"}),
+                dcc.Dropdown(id="location-dropdown",
+                            options=[{"label": j, "value": i} for i, j in select_columns.items()],
+                            value="casosNovos",
+                            style={"margin-top": "10px"}
+                            ),
+                dcc.Graph(id="line-graph", figure=fig2)
+            ])
+
+        ], md=5, style={"padding": "25px", "background-color": "#242424"}),
+
         dbc.Col([
             dcc.Graph(id="choropleth-map", figure=fig)
-        ])
+        ], md=7)
     ])
-)
+, fluid=True)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
