e3b24122ad92c045c1182ec0b1073aa02819b1a9
Juliano Augusto Da Silva Ambrosio <gestaojuliano99@gmail.com>
2024-11-05 10:19:01 -0300

    finalizado analise de dados da covid

--- index.py
+++ index.py
@@ -1,69 +1,175 @@
-import dash                                                     #Gerencia a dashboard e cria o servidor 
-from dash import dcc                                            #resonsavel por todos os componentes uteis na dash
-from dash import html                                           #Permite colocar codigos html na dash
-from dash.dependencies import Input, Output                     #Permite interação com o usuario
-import dash_bootstrap_components as dbc                         #Permite utilizar html,css e JS para formatar a interface web
+import dash  # Gerencia a dashboard e cria o servidor
+from dash import dcc  # Responsável por todos os componentes úteis na dash
+from dash import html  # Permite colocar códigos HTML na dash
+from dash.dependencies import Input, Output  # Permite interação com o usuário
+import dash_bootstrap_components as dbc  # Permite utilizar HTML, CSS e JS para formatar a interface web
 
+import plotly.express as px  # Permite criação dos gráficos dos plotly de forma mais fácil
+import plotly.graph_objects as go  # Nos dá mais controle sobre as ferramentas plotly
 
-import plotly.express as px                                     #Permite criação dos graficos dos plotly de forma mais faceis
-import plotly.graph_objects as go                               #Nos da mais controle sobre as ferranentas plotly
-
-import numpy as np                                              #Oferece  suporte oara array, coleção de funções matematicas etc.
-import json                                                     #Para leitura de dados ".json"
+import numpy as np  # Oferece suporte para array, coleção de funções matemáticas etc.
+import json  # Para leitura de dados ".json"
 import pandas as pd
 
-
-#Para ler o csv
-#df = pd.read_csv('/home/juliano/Documentos/projeto/Python/Dashboard/COVID-19/HIST_PAINEL_COVIDBR_13mai2021.csv', sep= ';')
-
-#le apenas as info do dataframe que contem estado
-#df_states = df[(~df["estado"].isna()) & (df["codmun"].isna())]
-
-#le apenas as info do dataframe que contem estado
-#df_brasil = df[df["regiao"] == "Brasil"]
-
-#Para ler o csv
+# =================================== Para ler o CSV
 df_states = pd.read_csv("df_states.csv")
 df_brasil = pd.read_csv("df_brasil.csv")
 
 df_states_ = df_states[df_states["data"] == "2020-05-13"]
+df_data = df_states[df_states["estado"] == "RJ"]
 
+select_columns = {
+    "casosAcumulado": "Casos Acumulados",
+    "casosNovos": "Novos Casos",
+    "obitosAcumulado": "Óbitos Totais",
+    "obitosNovos": "Óbitos por dia"
+}
 
-#le o geojason
-brazil_states = json.load(open("geojson/brazil_geo.json", "r"))
+# Lê o GeoJSON
+with open("geojson/brazil_geo.json", "r") as f:
+    brazil_states = json.load(f)
 
-
-#==================================================================================================
-#----------------------------Iniciando a criação da dash
-
+# =========================== Iniciando a criação da dash
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
-#Contem o mapa mais moderno, e melhor para estilizar
-fig = px.choropleth_mapbox(df_states_, locations= "estado", color= "casosNovos",
-                            center={"lat": -16.95, "lon": -47.78}, zoom= 4,
-                            geojson= brazil_states, color_continuous_scale= "Redor",
-                            opacity= 0.4, hover_data={"casosAcumulado": True, "casosNovos": True, 
-                            "obitosNovos": True, "estado": True})
+# ===========================Contém o mapa mais moderno, e melhor para estilizar
+fig = px.choropleth_mapbox(df_states_, locations="estado", color="casosNovos",
+                           center={"lat": -16.95, "lon": -47.78}, zoom=4,
+                           geojson=brazil_states, color_continuous_scale="Redor",
+                           opacity=0.4, hover_data={"casosAcumulado": True, "casosNovos": True,
+                                                    "obitosNovos": True, "estado": True})
 
 fig.update_layout(
-    paper_bgcolor = "#242424",
-    autosize = True,
-    margin = go.Margin(l=0, r=0, t=0, b=0),
-    showlegend = False,
-    mapbox_style = "carto-darkmatter"
+    paper_bgcolor="#242424",
+    autosize=True,
+    margin=dict(l=0, r=0, t=0, b=0),
+    showlegend=False,
+    mapbox_style="carto-darkmatter"
 )
 
-#==================================================================================================
-#----------------------------Layout
+# Criação da figura
+fig2 = go.Figure(layout={"template": "plotly_dark"})
 
+fig2.add_trace(go.Scatter(x=df_data["data"], y=df_data["casosAcumulado"]))
+fig2.update_layout(
+    paper_bgcolor="#242424",
+    plot_bgcolor="#242424",
+    autosize=True,
+    margin=dict(l=10, r=10, t=10, b=10)
+)
+
+# ======================================== Layout da aplicação
 app.layout = dbc.Container(
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id= "choropleth-map", figure= fig)
-        ])
-    ])
+            html.Div([
+                html.H1("Uma análise de dados sobre"),
+                html.H5("A evolução COVID-19"),
+                dbc.Button("Brasil", color="primary", id="location-button", size="lg")
+            ], style={"text-align": "center"}),
+            html.P("Informe a data na qual deseja obter informações:", style={"margin-top": "40px"}),
+            html.Div(id="div-test", children=[
+                dcc.DatePickerSingle(
+                    id="date-picker",
+                    min_date_allowed=df_brasil["data"].min(),
+                    max_date_allowed=df_brasil["data"].max(),
+                    initial_visible_month=df_brasil["data"].min(),
+                    date=df_brasil["data"].max(),
+                    display_format="MMMM D, YYYY",
+                    style={"border": "0px solid black"}
+                )
+            ]),
+
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.Span("Casos recuperados"),
+                            html.H6(style={"color": "#adfc92"}, id="casos-recuperados-text"),
+                            html.Span("Em acompanhamento"),
+                            html.H5(id="em-acompanhamento-text"),
+                        ])
+                    ], color="light", outline=True, style={"margin-top": "10px",
+                                                           "box-shadow": "0 4px 4px 0 rgba(0, 0, 0.15), 0 4px 20px 0 rgba(0, 0, 0, 0.19)",
+                                                           "color": "#FFFFFF"})
+                ], md=4),
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.Span("Total de casos confirmados"),
+                            html.H6(style={"color": "#389fd6"}, id="casos-confirmados-text"),
+                            html.Span("Novos casos na data"),
+                            html.H5(id="novos-casos-text"),
+                        ])
+                    ], color="light", outline=True, style={"margin-top": "10px",
+                                                           "box-shadow": "0 4px 4px 0 rgba(0, 0, 0.15), 0 4px 20px 0 rgba(0, 0, 0, 0.19)",
+                                                           "color": "#FFFFFF"})
+                ], md=4),
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            html.Span("Óbitos confirmados"),
+                            html.H6(style={"color": "#DF2935"}, id="obitos-text"),
+                            html.Span("Óbitos na data"),
+                            html.H5(id="obitos-na-data-text"),
+                        ])
+                    ], color="light", outline=True, style={"margin-top": "10px",
+                                                           "box-shadow": "0 4px 4px 0 rgba(0, 0, 0.15), 0 4px 20px 0 rgba(0, 0, 0, 0.19)",
+                                                           "color": "#FFFFFF"})
+                ], md=4),
+            ]),
+
+            html.Div([
+                html.P("Selecione o tipo de informação:", style={"margin-top": "25px"}),
+                dcc.Dropdown(id="location-dropdown",
+                             options=[{"label": j, "value": i} for i, j in select_columns.items()],
+                             value="casosNovos",
+                             style={"margin-top": "10px"}
+                             ),
+                dcc.Graph(id="line-graph", figure=fig2)
+            ])
+
+        ], md=5, style={"padding": "25px", "background-color": "#242424"}),
+
+        dbc.Col([
+            dcc.Loading(id="loading-1", type="default",
+                        children=[
+                            dcc.Graph(id="choropleth-map", figure=fig,
+                                      style={"height": "100vh", "margin-right": "10px"})
+                        ]
+                        ),
+        ], md=7, style={"padding-right": "0px", "padding-left": "0px", })
+    ], style={"margin-right": "0px", "margin-left": "0px"})
+    , fluid=True)
+
+
+# ====================================== Interatividade
+
+@app.callback(
+    [
+        Output("casos-recuperados-text", "children"),
+        Output("em-acompanhamento-text", "children"),
+        Output("casos-confirmados-text", "children"),
+        Output("novos-casos-text", "children"),
+        Output("obitos-text", "children"),
+        Output("obitos-na-data-text", "children")
+    ],
+    [Input("date-picker", "date"), Input("location-button", "children")]
 )
+def display_status(date, location):
+    if location == "Brasil":
+        df_data_on_date = df_brasil[df_brasil["data"] == date]
+    else:
+        df_data_on_date = df_states[(df_states["estado"] == location) & (df_states["data"] == date)]
+
+    recuperados_novos = "-" if df_data_on_date["Recuperadosnovos"].isna().values[0] else f'{int(df_data_on_date["Recuperadosnovos"].values[0]):,}'.replace(",", ".")
+    acompanhamentos_novos = "-" if df_data_on_date["emAcompanhamentoNovos"].isna().values[0] else f'{int(df_data_on_date["emAcompanhamentoNovos"].values[0]):,}'.replace(",", ".")
+    casos_acumulados = "-" if df_data_on_date["casosAcumulado"].isna().values[0] else f'{int(df_data_on_date["casosAcumulado"].values[0]):,}'.replace(",", ".")
+    casos_novos = "-" if df_data_on_date["casosNovos"].isna().values[0] else f'{int(df_data_on_date["casosNovos"].values[0]):,}'.replace(",", ".")
+    obitos_acumulados = "-" if df_data_on_date["obitosAcumulado"].isna().values[0] else f'{int(df_data_on_date["obitosAcumulado"].values[0]):,}'.replace(",", ".")
+    obitos_novos = "-" if df_data_on_date["obitosNovos"].isna().values[0] else f'{int(df_data_on_date["obitosNovos"].values[0]):,}'.replace(",", ".")
+    return (recuperados_novos, acompanhamentos_novos, casos_acumulados, casos_novos, obitos_acumulados, obitos_novos)
 
 
 if __name__ == "__main__":
-    app.run_server(debug = True)
+    app.run_server(debug=True)
